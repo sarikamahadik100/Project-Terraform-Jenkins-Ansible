f7f0289494a5d349ec982575de9f368c9b2fe04d
sarikamahadik100 <sarikamahadik100@gmail.com>
2026-07-12 20:46:56 +0530

    Add Terraform configuration for AWS instances

---
+++ createInstance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 3
+  ami           = "ami-0b6d9d3d33ba97d99"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "demoinstnce-${count.index}"
+  }
+}
