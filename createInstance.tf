0ff9026158d770c37a0a91836148be53101e0c18
sarikamahadik100 <sarikamahadik100@gmail.com>
2026-07-13 17:53:55 +0530

    Refactor user_data for instance creation
    Updated user_data to use templatefile for userdata script.

--- createInstance.tf
+++ createInstance.tf
@@ -7,10 +7,13 @@ resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"
 
- key_name = "ansible-key"
+  key_name = "ansible-key"
 
-  # Install Ansible only on the first instance
-  user_data = count.index == 0 ? file("${path.module}/ansible-install.sh") : null
+  user_data = count.index == 0 ?
+    file("${path.module}/ansible-install.sh") :
+    templatefile("${path.module}/remote-userdata.sh", {
+      PUBLIC_KEY = local.public_key
+    })
 
   tags = {
     Name = count.index == 0 ? "Control-Server" : "Remote-Server-${count.index}"
